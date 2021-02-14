81aac1b205731c4c17d65eb2a18b8ba178f7b1e0
FancyFennec <jonasallemann@gmx.ch>
2021-02-14 20:52:26 +0100

    added everything

---
+++ Assets/Scripts/AssetConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AssetConsumer : MonoBehaviour
+{
+
+	public List<Vector3> verteces = new List<Vector3>();
+	List<GameObject> gameObjects = new List<GameObject>();
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
