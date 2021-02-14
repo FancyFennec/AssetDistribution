7acd9a415f0523e2ab9888d6cafe403545d46151
FancyFennec <jonasallemann@gmx.ch>
2021-02-14 21:12:42 +0100

    started to implement the asset distribution

--- Assets/Scripts/AssetConsumer.cs
+++ Assets/Scripts/AssetConsumer.cs
@@ -5,9 +5,7 @@ using System.Linq;
 
 public class AssetConsumer : MonoBehaviour
 {
-
-	public List<Vector3> verteces = new List<Vector3>();
-	List<GameObject> gameObjects = new List<GameObject>();
+	public List<DistributedAsset> distributedAssets = new List<DistributedAsset>();
 
 	void Start()
 	{
@@ -18,4 +16,20 @@ public class AssetConsumer : MonoBehaviour
 	{
 
 	}
+}
+
+public class DistributedAsset
+{
+	public static string RandomString(int length)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+		  .Select(s => s[Random.Range(0, chars.Length)]).ToArray());
+	}
+
+	public string name = RandomString(12);
+	public List<Vector3> verteces = new List<Vector3>();
+	public List<GameObject> spawnables = new List<GameObject>();
+	public float density = 2f;
+	public int currentVertexIndex = 0;
 }
